7d96608af2316710b8c1d959c448b1a5cf56096a
Petros295 <petrpetrosyan2005@gmail.com>
2024-10-21 20:06:27 +0300

    Add files via upload

---
+++ Pap4/Pap3/Pap2/calculator.js
@@ -0,0 +1,81 @@
+﻿function updatePrice() {
+    let s = document.getElementsByName("type");
+    let select = s[0];
+    let price = 0;
+    let prices = {
+        types: [1500000, 1600000, 1700000,1850000],
+        options: {
+            1:20000,
+            3:40000,
+        },
+        checkboxes: {
+            1: 25000,
+            2: 15000,
+            3: 7000,
+        }
+    };
+    price = prices.types[select.value - 1];
+    let radioDiv = document.getElementById("radios");
+    radioDiv.style.display = (select.value == "2" ? "block" : "none");
+    let radios = document.getElementsByName("options");
+    radios.forEach(function(radio) {
+        if (radio.checked) {
+            let optionPrice = prices.options[radio.value];
+            if (optionPrice !== undefined) {
+                price += optionPrice;
+            }
+        }
+    });
+    let checkDiv = document.getElementById("checkboxes");
+    checkDiv.style.display = (select.value == "1" || select.value == "2" ? "none" : "block");
+    let checkboxes = document.querySelectorAll("#checkboxes input");
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            let cPrice = prices.checkboxes[checkbox.name];
+            price += cPrice;
+        }
+    });
+    let count = document.getElementById("count").value;
+    if(parseInt(count) < 0) {
+        let Price = document.getElementById("price");
+        Price.innerHTML = "Данные введены неправильно";
+    }
+    else {
+        price += (count*5000);
+        if(select.value != "2") 
+        {  
+            if(select.value == "1") {
+                price = prices.types[0] + (count*5000);
+            }
+        }
+        
+        let Price = document.getElementById("price");
+        Price.innerHTML = price + " рублей";
+    }
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+    let radioDiv = document.getElementById("radios");
+    radioDiv.style.display = "none";
+    let s = document.getElementsByName("type");
+    let select = s[0];
+    select.addEventListener("change", function(event) {
+        updatePrice();
+    });
+    let count = document.getElementById("count");
+    count.addEventListener("change", function(event) {
+        updatePrice();
+    });
+    let radios = document.getElementsByName("options");
+    radios.forEach(function(radio) {
+        radio.addEventListener("change", function(event) {
+            updatePrice();
+        });
+    });
+    let checkboxes = document.querySelectorAll("#checkboxes input");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener("change", function(event) {
+            updatePrice();
+        });
+    });
+    updatePrice();
+});
